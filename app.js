0db3d841bd9e772e90cb3564712910306e928b45
karen-ifrt <55798470+karen-ifrt@users.noreply.github.com>
2020-06-13 17:59:29 +0200

    timer

--- app.js
+++ app.js
@@ -1,6 +1,87 @@
-function Welcome({name}) {
-    return <h1>Bonjour {name}</h1>
+function WelcomeFunc({name, children}) {
+    return <div><h1>Bonjour {name}</h1>
+        <p>{children}</p>
+    </div>
+}
+
+class Welcome extends React.Component {
+
+    render() {
+        return <div><h1>Bonjour {this.props.name}</h1>
+            <p>{this.props.children}</p>
+        </div>
+    }
 }
 
 
-ReactDOM.render(<Welcome name="Karen" />, document.querySelector('#app'))
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {date: new Date()}
+        this.timer = null
+    }
+
+    componentDidMount() {
+        this.timer = window.setInterval(this.tick.bind(this), 1000)
+    }
+
+    componentWillUnmont() {
+        window.clearInterval(this.timer)
+    }
+
+    tick() {
+        this.setState({date: new Date()})
+    }
+
+    render() {
+        return <div>
+            Il est {this.state.date.toLocaleDateString()} {this.state.date.toLocaleTimeString()}
+        </div>
+    }
+}
+
+class Incrementer extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { n: props.start }
+        this.timer = null
+    }
+    
+    componentDidMount() {
+        window.setInterval(this.plus.bind(this), 1000)
+    }
+
+    componentWillUnmont() {
+        window.clearInterval(this.timer)
+    }
+
+    plus() {
+        // Si plusieurs setState : mettre dans une fonction
+        this.setState((state, props) => ({n: state.n + props.step}))
+    }
+    
+    render() {
+        return <div>Vous êtes en ligne depuis {this.state.n} secondes</div>
+    }
+}
+
+Incrementer.defaultProps = {
+    start: 0,
+    step: 1
+}
+
+
+function Home() {
+    return <div>
+        <Welcome name="Karen" />
+        <Welcome name="Romain" />
+        <Clock />
+        <Incrementer start={10} />
+        <Incrementer start={100} step={10} />
+    </div>
+}
+
+
+ReactDOM.render(<Home/>, document.querySelector('#app'))
